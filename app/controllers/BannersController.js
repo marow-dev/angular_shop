779e557a81c3c349e1ac38930f9d1fdb8369333b
marow-dev <marow.dev@gmail.com>
2016-04-25 21:51:34 +0200

    code formatting

--- app/controllers/BannersController.js
+++ app/controllers/BannersController.js
@@ -1,10 +1,9 @@
-(function() {
-	angular.module('shopApp').controller('BannersController', ['BannersModel',
-		function(BannersModel) {
-			var banners = this;
-			banners.list = [];
-
-			BannersModel.load().then(function(result) { banners.list = result; });
-		}]
-	);
-})();
+(function () {
+    'use strict';
+    angular.module('shopApp').controller('BannersController', ['BannersModel',
+        function (BannersModel) {
+            var banners = this;
+            banners.list = [];
+            BannersModel.load().then(function (result) { banners.list = result; });
+        }]);
+}());
