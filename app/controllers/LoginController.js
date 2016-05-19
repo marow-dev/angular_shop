021c14b34bc86afd7e25da8bc79fcee05556d820
marow-dev <marow.dev@gmail.com>
2016-05-19 21:48:50 +0200

    change to main page after log in

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
-    angular.module('shopApp').controller('LoginController', ['$scope', 'UserModel',
-        function ($scope, UserModel) {
+    angular.module('shopApp').controller('LoginController', ['$scope', '$location', 'UserModel',
+        function ($scope, $location, UserModel) {
             var login = this;
 
             login.userdata = {};
@@ -9,6 +9,7 @@
                 if (isValid) {
                     UserModel.login(userdata).then(function (user) {
                         $scope.user.data.setUser({name: user.name, login: user.login});
+                        $location.path('/');
                     }).catch(function (err) {
                         login.form.login.$setValidity('login', false);
                         login.form.pass.$setValidity('login', false);
